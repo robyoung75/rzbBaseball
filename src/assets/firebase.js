6193d081d8718c6514856c35ac0683495f8fee41
robyoung75 <robyoug75@gmail.com>
2020-10-20 19:31:46 -0700

    intitial commit

---
+++ src/assets/firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from 'firebase';
+import 'firebase/storage';
+
+import firebaseConfig from '../API/api';
+
+// initialize the firebase database
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// realtime data base in firestore
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebaseApp.storage();
+
+export { db, auth, storage };
+
+
+
