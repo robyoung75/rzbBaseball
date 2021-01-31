897953484497f59185e67dc2fc6d7dcd87fe3067
robyoung75 <robyoug75@gmail.com>
2021-01-31 13:28:54 -0700

    gamechanger iframe

--- src/Components/GameChanger/GameChanger.js
+++ src/Components/GameChanger/GameChanger.js
@@ -4,15 +4,13 @@ import "./GameChanger.css";
 function GameChanger() {
   return (
     <div className="gameChanger">
-      <h3>Scores</h3>
       <div className="gameChanger__iFrame">
         <iframe
-          src="//gc.com/scoreboard-5f88a82dba69b7fcaebbeca7?g=3&p=58ac7803378dca001a989c9c"
-          width="500"
-          height="110"
+          src="//gc.com/scoreboard-5f88a82dba69b7fcaebbeca7?g=5&p=58ac7803378dca001a989c9c"
+          width="728"
+          height="90"
           scrolling="no"
-          frameBorder="0"
-          title="game_changer"
+          frameborder="0"
         ></iframe>
       </div>
     </div>
--- src/Components/StoryReel/StoryReel.js
+++ src/Components/StoryReel/StoryReel.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Story from "../Story/Story";
 import "./StoryReel.css";
+import GameChanger from '../GameChanger/GameChanger'
 
 function StoryReel({mobile}) {
   return (
     <div className="storyReel">
-        
+       {/* <GameChanger/> */}
       <h3>Players of the Week</h3>
-      <div className="storyReel__story">      
+      <div className="storyReel__story">     
+      
         <Story />
         <Story />
         <Story />
       </div>
+      <GameChanger/>
     </div>
   );
 }
